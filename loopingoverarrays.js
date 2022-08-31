7bf74de19fe0cbe23bdc6ee67af5519abbffb078
zachmanac <zachstephenson94@gmail.com>
2022-08-31 23:34:14 +0000

    loopingoverarrays.js

--- loopingoverarrays.js
+++ loopingoverarrays.js
@@ -5,4 +5,4 @@ for (let amount of amounts) {
   total += amount;
 }
 console.log('Order total is: ', total);
-//for...of loop. Useful if going in order of whole array, not useful if wanting different order ex: 52.25, 61, 112.99, 5, will have to use regular for loop.
+//for...of loop. Useful if going in order of whole array, not useful if wanting different order ex: 52.25, 61, 112.99, 5, will have to use the regular for loop.
