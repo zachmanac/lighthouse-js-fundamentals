14e4267fdd8311a86b7d2ddfc34805d4abdefd5a
zachmanac <zachstephenson94@gmail.com>
2022-08-31 17:59:38 +0000

    testing

---
+++ conditionals.js
@@ -0,0 +1,30 @@
+const temperature = 45;
+
+if (temperature < 0) {
+  console.log("Make sure you pick out a scarf!");
+} else if (temperature < 15) {
+  console.log("Short sleeves won't cut it!");
+} else {
+  console.log("Short sleeves are fine.");
+}
+
+console.log("Now you're ready to go outside!");
+
+const isCitizen = true;
+const age = 26;
+
+if (isCitizen && age > 18) {
+  console.log("You are eligible to vote.");
+}
+
+if (temperature < -40 || temperature > 40) {
+  console.log("Maybe going outside isn't such a great idea…");
+}
+
+const raining = false
+
+if (!raining) {
+  console.log("Leave your umbrella at home!");
+}
+
+
