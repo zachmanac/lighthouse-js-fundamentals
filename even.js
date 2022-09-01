5af6462903c702783265e762954eb1e6cc4d77da
zachmanac <zachstephenson94@gmail.com>
2022-09-01 03:35:33 +0000

    even.js

---
+++ even.js
@@ -0,0 +1,13 @@
+const isEven  = function (num) {
+  return num % 2 === 0;
+}
+
+const tenIsEven = isEven(10);
+const elevenIsEven = isEven(11);
+
+console.log(tenIsEven);
+console.log(elevenIsEven);
+
+console.log(isEven(10));
+console.log(isEven(11));
+//shortcut for lines 5-9
