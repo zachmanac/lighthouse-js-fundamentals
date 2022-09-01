4702c19adda4c8d35d9999ba210eb382c4501ab0
zachmanac <zachstephenson94@gmail.com>
2022-09-01 03:30:09 +0000

    hello.js

--- hello.js
+++ hello.js
@@ -6,3 +6,9 @@ sayHello("Caliban");
 sayHello("Miranda");
 sayHello("Ferdinand");
 
+const returnSayHello  = function (name) {
+  return "Hello, " + name;
+}
+const greeting = returnSayHello('John');
+console.log(greeting);
+
