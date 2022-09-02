a485512998be1acc00bd991cfe75b6da1cbf1701
zachmanac <zachstephenson94@gmail.com>
2022-09-02 03:19:11 +0000

    --all

--- packing-list.js
+++ packing-list.js
@@ -1,6 +1,15 @@
 const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
 
 console.log("Kitchen stuff to pack:");
+let i = 0;
+while (i < packingList.length) {
+  console.log(packingList[i]);
+  i++;
+}
+
+console.log("Kitchen stuff to pack:");
 for (let i = 0; i < packingList.length; i++) {
   console.log(packingList[i]);
 }
+//difference between for and while loops, giving the same output
+
